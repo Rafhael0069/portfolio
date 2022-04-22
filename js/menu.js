d1dedc0853b76bd127caebd657ad395c379e77fb
RafhaelGaspar <86062361+RafhaelGaspar@users.noreply.github.com>
2022-04-22 10:28:48 -0300

    responsividade concluida

---
+++ js/menu.js
@@ -0,0 +1,47 @@
+class MobileNavbar {
+  constructor(menuMobile, ulNavegacao, navLinks) {
+    this.menuMobile = document.querySelector(menuMobile);
+    this.ulNavegacao = document.querySelector(ulNavegacao);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.ulNavegacao.classList.toggle(this.activeClass);
+    this.menuMobile.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.menuMobile.addEventListener("click", this.handleClick);
+    this.navLinks.forEach((link) => {
+      link.addEventListener("click", this.handleClick);
+    });
+  }
+
+  init() {
+    if (this.menuMobile) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".menu-mobile",
+  ".ul-navegacao",
+  ".ul-navegacao li"
+);
+mobileNavbar.init();
